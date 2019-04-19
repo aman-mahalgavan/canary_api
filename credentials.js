ca08b8f7d0539853f06bc9e3d26b9aa5ef66b8ad
Ankit Brahmbhatt <ankitbrahmbhatt1997@gmail.com>
2019-04-19 12:33:14 +0530

    config vars added

---
+++ credentials.js
@@ -0,0 +1,10 @@
+module.exports = {
+  sendGridApiKey:
+    "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",
+  googleClientId:
+    process.env.googleClientId,
+  accessToken:
+    process.env.accessToken,
+  refreshToken: process.env.refreshToken,
+  user: "ukogdna6560@gmail.com"
+};
