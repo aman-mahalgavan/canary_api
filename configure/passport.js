a9f05f41861bcbefa18f6390ecbda8477ea2b03e
Ankit Brahmbhatt <ankitbrahmbhatt1997@gmail.com>
2019-04-19 12:16:33 +0530

    keys fixed

--- configure/passport.js
+++ configure/passport.js
@@ -1,11 +1,11 @@
 const passport = require("passport");
 const jwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
-const { tokenSECRET } = require("./keys");
+const keys = require("./keys");
 const User = require("../Models/User");
 let opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = tokenSECRET;
+opts.secretOrKey = keys.tokenSECRET;
 
 module.exports = () => {
   passport.use(
